d71747597ef89d786e740073b7dc7d32c0729d96
Dion <dd68096@ubt-uni.net>
2025-01-23 01:29:34 +0100

    PetShop-24-Added CountryService and CityService

---
+++ PetShop.Application/Services/CityService.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.Extensions.Logging;
+using PetShop.Application.Interfaces;
+using PetShop.Domain.Common;
+using PetShop.Domain.Entities;
+using PetShop.Domain.Interfaces;
+
+namespace PetShop.Application.Services;
+
+public class CityService: ICityService
+{
+    private readonly ICityRepository _cityRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly ILogger<CityService> _logger;
+
+    public CityService(ICityRepository cityRepository, IUnitOfWork unitOfWork, ILogger<CityService> logger)
+    {
+        _cityRepository = cityRepository;
+        _unitOfWork = unitOfWork;
+        _logger = logger;
+    }
+
+    public async Task<Result<bool>> CreateCityAsync(City city)
+    {
+        try
+        {
+            _logger.LogInformation("Started creating a City with Id: {CityId}", city.Id);
+
+            await _cityRepository.InsertAsync(city);
+            var cityCreated = await _unitOfWork.SaveChangesAsync() > 0;
+
+            if (cityCreated)
+            {
+                _logger.LogInformation("Successfully created a City with Id: {CityId}", city.Id);
+                return Result<bool>.Success();
+            }
+
+            _logger.LogWarning("Failed to create a City with Id: {CityId}", city.Id);
+            return Result<bool>.Failure(CityErrors.CreationFailed);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred in the {ServiceName} while attempting to create a City with Id: {CityId}",
+                             nameof(CityService), city.Id);
+            return Result<bool>.Failure(CityErrors.CreationUnexpectedError);
+        }
+    }
+
+    public async Task<Result<List<City>>> GetAllCitiesAsync()
+    {
+        try
+        {
+            _logger.LogInformation("Started retrieving all cities.");
+
+            var cities = await _cityRepository.GetAllAsync();
+            if (cities is null)
+            {
+                _logger.LogInformation("Successfully retrieved all cities.");
+                return Result<List<City>>.Failure(CityErrors.RetrievalError);
+            }
+
+            _logger.LogInformation("Successfully retrieved all cities.");
+            return Result<List<City>>.Success(cities.ToList());
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred in the {ServiceName} while attempting to retrieve all cities.", nameof(CityService));
+            return Result<List<City>>.Failure(CityErrors.RetrievalError);
+        }
+    }
+
+    public async Task<Result<City>> GetCityByIdAsync(int cityId)
+    {
+        try
+        {
+            _logger.LogInformation("Started retrieving City with Id: {CityId}", cityId);
+
+            var city = await _cityRepository.GetByIdAsync(cityId);
+
+            if (city is null)
+            {
+                _logger.LogWarning("City with Id: {CityId} was not found.", cityId);
+                return Result<City>.Failure(CityErrors.NotFound(cityId));
+            }
+
+            _logger.LogInformation("Successfully retrieved City with Id: {CityId}", cityId);
+            return Result<City>.Success(city);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred in the {ServiceName} while attempting to retrieve City with Id: {CityId}",
+                             nameof(CityService), cityId);
+            return Result<City>.Failure(CityErrors.RetrievalError);
+        }
+    }
+
+    public async Task<Result<bool>> UpdateCityAsync(City city)
+    {
+        try
+        {
+            _logger.LogInformation("Started updating City with Id: {CityId}", city.Id);
+
+            await _cityRepository.UpdateAsync(city);
+            var cityUpdated = await _unitOfWork.SaveChangesAsync() > 0;
+
+            if (cityUpdated)
+            {
+                _logger.LogInformation("Successfully updated City with Id: {CityId}", city.Id);
+                return Result<bool>.Success();
+            }
+
+            _logger.LogWarning("Failed to update City with Id: {CityId}. No changes were detected.", city.Id);
+            return Result<bool>.Failure(CityErrors.NoChangesDetected);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred in the {ServiceName} while attempting to update City with Id: {CityId}",
+                             nameof(CityService), city.Id);
+            return Result<bool>.Failure(CityErrors.UpdateUnexpectedError);
+        }
+    }
+
+    public async Task<Result<bool>> DeleteCityAsync(int cityId)
+    {
+        try
+        {
+            _logger.LogInformation("Started deleting City with Id: {CityId}", cityId);
+
+            var city = await _cityRepository.GetByIdAsync(cityId);
+
+            if (city is null)
+            {
+                _logger.LogWarning("City with Id: {CityId} was not found.", cityId);
+                return Result<bool>.Failure(CityErrors.NotFound(cityId));
+            }
+
+            await _cityRepository.DeleteAsync(city);
+            var cityDeleted = await _unitOfWork.SaveChangesAsync() > 0;
+
+            if (cityDeleted)
+            {
+                _logger.LogInformation("Successfully deleted City with Id: {CityId}", cityId);
+                return Result<bool>.Success();
+            }
+
+            _logger.LogWarning("Failed to delete City with Id: {CityId}. No changes were detected.", cityId);
+            return Result<bool>.Failure(CityErrors.NoChangesDetected);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred in the {ServiceName} while attempting to delete City with Id: {CityId}",
+                             nameof(CityService), cityId);
+            return Result<bool>.Failure(CityErrors.DeletionUnexpectedError);
+        }
+    }
+}
---
+++ PetShop.Application/Services/CountryService.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.Extensions.Logging;
+using PetShop.Application.Interfaces;
+using PetShop.Domain.Common;
+using PetShop.Domain.Entities;
+using PetShop.Domain.Interfaces;
+
+namespace PetShop.Application.Services;
+
+public class CountryService: ICountryService
+{
+    private readonly ICountryRepository _countryRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly ILogger<CountryService> _logger;
+
+    public CountryService(ICountryRepository countryRepository, IUnitOfWork unitOfWork, ILogger<CountryService> logger)
+    {
+        _countryRepository = countryRepository;
+        _unitOfWork = unitOfWork;
+        _logger = logger;
+    }
+
+    public async Task<Result<bool>> CreateCountryAsync(Country country)
+    {
+        try
+        {
+            _logger.LogInformation("Started creating a Country with Id: {CountryId}", country.Id);
+
+            await _countryRepository.InsertAsync(country);
+            var countryCreated = await _unitOfWork.SaveChangesAsync() > 0;
+
+            if (countryCreated)
+            {
+                _logger.LogInformation("Successfully created a Country with Id: {CountryId}", country.Id);
+                return Result<bool>.Success();
+            }
+
+            _logger.LogWarning("Failed to create a Country with Id: {CountryId}", country.Id);
+            return Result<bool>.Failure(CountryErrors.CreationFailed);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred in the {ServiceName} while attempting to create a Country with Id: {CountryId}",
+                             nameof(CountryService), country.Id);
+            return Result<bool>.Failure(CountryErrors.CreationUnexpectedError);
+        }
+    }
+
+    public async Task<Result<List<Country>>> GetAllCountriesAsync()
+    {
+        try
+        {
+            _logger.LogInformation("Started retrieving all countries.");
+
+            var countries = await _countryRepository.GetAllAsync();
+
+            if (countries is null)
+            {
+                _logger.LogError("No countries found");
+                return Result<List<Country>>.Failure(CountryErrors.RetrievalError);
+            }
+
+            _logger.LogInformation("Successfully retrieved all countries.");
+            return Result<List<Country>>.Success(countries.ToList());
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred in the {ServiceName} while attempting to retrieve all countries.", nameof(CountryService));
+            return Result<List<Country>>.Failure(CountryErrors.RetrievalError);
+        }
+    }
+
+    public async Task<Result<Country>> GetCountryByIdAsync(int countryId)
+    {
+        try
+        {
+            _logger.LogInformation("Started retrieving Country with Id: {CountryId}", countryId);
+
+            var country = await _countryRepository.GetByIdAsync(countryId);
+
+            if (country is null)
+            {
+                _logger.LogWarning("Country with Id: {CountryId} was not found.", countryId);
+                return Result<Country>.Failure(CountryErrors.NotFound(countryId));
+            }
+
+            _logger.LogInformation("Successfully retrieved Country with Id: {CountryId}", countryId);
+            return Result<Country>.Success(country);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred in the {ServiceName} while attempting to retrieve Country with Id: {CountryId}",
+                             nameof(CountryService), countryId);
+            return Result<Country>.Failure(CountryErrors.RetrievalError);
+        }
+    }
+
+    public async Task<Result<bool>> UpdateCountryAsync(Country country)
+    {
+        try
+        {
+            _logger.LogInformation("Started updating Country with Id: {CountryId}", country.Id);
+
+            await _countryRepository.UpdateAsync(country);
+            var countryUpdated = await _unitOfWork.SaveChangesAsync() > 0;
+
+            if (countryUpdated)
+            {
+                _logger.LogInformation("Successfully updated Country with Id: {CountryId}", country.Id);
+                return Result<bool>.Success();
+            }
+
+            _logger.LogWarning("Failed to update Country with Id: {CountryId}. No changes were detected.", country.Id);
+            return Result<bool>.Failure(CountryErrors.NoChangesDetected);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred in the {ServiceName} while attempting to update Country with Id: {CountryId}",
+                             nameof(CountryService), country.Id);
+            return Result<bool>.Failure(CountryErrors.UpdateUnexpectedError);
+        }
+    }
+
+    public async Task<Result<bool>> DeleteCountryAsync(int countryId)
+    {
+        try
+        {
+            _logger.LogInformation("Started deleting Country with Id: {CountryId}", countryId);
+
+            var country = await _countryRepository.GetByIdAsync(countryId);
+            if (country is null)
+            {
+                _logger.LogWarning("Country with Id: {CountryId} was not found.", countryId);
+                return Result<bool>.Failure(CountryErrors.NotFound(countryId));
+            }
+
+            await _countryRepository.DeleteAsync(country);
+            var countryDeleted = await _unitOfWork.SaveChangesAsync() > 0;
+
+            if (countryDeleted)
+            {
+                _logger.LogInformation("Successfully deleted Country with Id: {CountryId}", countryId);
+                return Result<bool>.Success();
+            }
+
+            _logger.LogWarning("Failed to delete Country with Id: {CountryId}. No changes were detected.", countryId);
+            return Result<bool>.Failure(CountryErrors.NoChangesDetected);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred in the {ServiceName} while attempting to delete Country with Id: {CountryId}",
+                             nameof(CountryService), countryId);
+            return Result<bool>.Failure(CountryErrors.DeletionUnexpectedError);
+        }
+    }
+}
