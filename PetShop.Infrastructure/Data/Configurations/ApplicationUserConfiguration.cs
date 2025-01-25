03f3df643fff9135c9c06c4497294dcb1b7fa357
Rigon <rigonmej@gmail.com>
2025-01-25 16:26:11 +0100

    PetShop-3-Moved Configurations folder in Data and added configurations

---
+++ PetShop.Infrastructure/Data/Configurations/ApplicationUserConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetShop.Domain.Entities;
+
+namespace PetShop.Infrastructure.Configurations;
+
+public class ApplicationUserConfiguration: IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder
+            .HasMany(u => u.Pets)
+            .WithOne(p => p.User)
+            .HasForeignKey(p => p.PostedByUserId);
+    }
+}
---
+++ PetShop.Infrastructure/Data/Configurations/BreedEntityTypeConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetShop.Domain.Entities;
+
+namespace PetShop.Infrastructure.Configurations;
+
+public class BreedEntityTypeConfiguration: IEntityTypeConfiguration<Breed>
+{
+    public void Configure(EntityTypeBuilder<Breed> builder)
+    {
+        builder
+            .HasOne(b => b.Species)
+            .WithMany(s => s.Breeds)
+            .HasForeignKey(b => b.SpeciesId);
+    }
+}
