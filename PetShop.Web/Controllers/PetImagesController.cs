e6387eaffe064b8a4e22f1ac438994b79a7ede2d
Dion <dd68096@ubt-uni.net>
2025-01-26 19:03:49 +0100

    PetShop-7-Created PetImagesController

---
+++ PetShop.Web/Controllers/PetImagesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetShop.Controllers;
+
+[Route("[controller]")]
+public class PetImagesController : Controller
+{
+    private readonly ILogger<PetImagesController> _logger;
+
+    public PetImagesController(ILogger<PetImagesController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View("Error!");
+    }
+}
